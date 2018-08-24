f299fd5781573b3cf64bda63b1ac7d13551e2b69
GatienPoirrier <gatien.poirrier@epfedu.fr>
2018-08-24 15:15:46 +0200

    Took off credentials, kp and change the us region

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-04681a1dbd79675a5"
+  ami           = "ami-0bdb1d6c15a40392c"
   instance_type = "t2.micro"
 
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
 
   associate_public_ip_address = "true"
 
-  key_name = "MyGuruKP"
+  key_name = "myTerraKP"
 
   user_data = "${data.template_file.userdata1.rendered}"
 
